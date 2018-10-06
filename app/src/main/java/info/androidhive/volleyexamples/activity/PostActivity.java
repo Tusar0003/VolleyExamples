2e15adcd5c26a0db1815e12af8f4e52656ac9319
Tusar0003 <faisalahmedtusar@gmail.com>
2018-10-06 14:14:38 +0600

    Mod: google book api added and working.

--- app/src/main/java/info/androidhive/volleyexamples/activity/PostActivity.java
+++ app/src/main/java/info/androidhive/volleyexamples/activity/PostActivity.java
@@ -34,62 +34,25 @@ public class PostActivity extends AppCompatActivity {
 
         final EditText nameEditText = (EditText) findViewById(R.id.name);
         final TextView responseTextView = (TextView) findViewById(R.id.msgResponse);
-//        findViewById(R.id.btnPost).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (!TextUtils.isEmpty(nameEditText.getText())) {
-//                    // Tag used to cancel the request
-//                    String tag_json_obj = "json_obj_req";
-//                    String url = String.format("https://www.googleapis.com/books/v1/volumes?q=%1$s",
-//                            nameEditText.getText().toString().trim());
-//
-//                    final ProgressDialog pDialog = new ProgressDialog(PostActivity.this);
-//                    pDialog.setMessage("Loading...");
-//                    pDialog.show();
-//
-//                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null,
-//                            new Response.Listener<JSONObject>() {
-//
-//                                @Override
-//                                public void onResponse(JSONObject response) {
-//                                    Log.e(TAG, response.toString());
-//                                    responseTextView.setText(response.toString());
-//                                    pDialog.hide();
-//                                }
-//                            }, new Response.ErrorListener() {
-//
-//                        @Override
-//                        public void onErrorResponse(VolleyError error) {
-//                            VolleyLog.e(TAG, "Error: " + error.getMessage());
-//                            pDialog.hide();
-//                        }
-//                    });
-//
-//                    // Adding request to request queue
-//                    AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
-//                }
-//            }
-//        });
-
         findViewById(R.id.btnPost).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 if (!TextUtils.isEmpty(nameEditText.getText())) {
                     // Tag used to cancel the request
                     String tag_json_obj = "json_obj_req";
-//                    String url = "https://api.androidhive.info/volley/person_object.json";
-                    String url = "http://httpbin.org/post";
+                    String url = String.format("https://www.googleapis.com/books/v1/volumes?q=%1$s",
+                            nameEditText.getText().toString().trim());
 
                     final ProgressDialog pDialog = new ProgressDialog(PostActivity.this);
                     pDialog.setMessage("Loading...");
                     pDialog.show();
 
-                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, url, null,
+                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null,
                             new Response.Listener<JSONObject>() {
 
                                 @Override
                                 public void onResponse(JSONObject response) {
-                                    Log.d(TAG, response.toString());
+                                    Log.e(TAG, response.toString());
                                     responseTextView.setText(response.toString());
                                     pDialog.hide();
                                 }
@@ -100,31 +63,68 @@ public class PostActivity extends AppCompatActivity {
                             VolleyLog.e(TAG, "Error: " + error.getMessage());
                             pDialog.hide();
                         }
-                    }) {
-
-                        @Override
-                        protected Map<String, String> getParams() {
-                            Log.e(TAG, "getParams: called");
-
-//                            Map<String, String> params = new HashMap<String, String>();
-//                            params.put("name", "Androidhive");
-//                            params.put("email", "abc@androidhive.info");
-//                            params.put("password", "password123");
-
-                            Map<String, String>  params = new HashMap<String, String>();
-                            params.put("name", "Alif");
-                            params.put("domain", "http://itsalif.info");
-                            Log.e(TAG, "getParams: params: " + params);
-
-                            return params;
-                        }
-
-                    };
+                    });
 
                     // Adding request to request queue
                     AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
                 }
             }
         });
+
+//        findViewById(R.id.btnPost).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!TextUtils.isEmpty(nameEditText.getText())) {
+//                    // Tag used to cancel the request
+//                    String tag_json_obj = "json_obj_req";
+////                    String url = "https://api.androidhive.info/volley/person_object.json";
+//                    String url = "http://httpbin.org/post";
+//
+//                    final ProgressDialog pDialog = new ProgressDialog(PostActivity.this);
+//                    pDialog.setMessage("Loading...");
+//                    pDialog.show();
+//
+//                    JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, url, null,
+//                            new Response.Listener<JSONObject>() {
+//
+//                                @Override
+//                                public void onResponse(JSONObject response) {
+//                                    Log.d(TAG, response.toString());
+//                                    responseTextView.setText(response.toString());
+//                                    pDialog.hide();
+//                                }
+//                            }, new Response.ErrorListener() {
+//
+//                        @Override
+//                        public void onErrorResponse(VolleyError error) {
+//                            VolleyLog.e(TAG, "Error: " + error.getMessage());
+//                            pDialog.hide();
+//                        }
+//                    }) {
+//
+//                        @Override
+//                        protected Map<String, String> getParams() {
+//                            Log.e(TAG, "getParams: called");
+//
+////                            Map<String, String> params = new HashMap<String, String>();
+////                            params.put("name", "Androidhive");
+////                            params.put("email", "abc@androidhive.info");
+////                            params.put("password", "password123");
+//
+//                            Map<String, String>  params = new HashMap<String, String>();
+//                            params.put("name", "Alif");
+//                            params.put("domain", "http://itsalif.info");
+//                            Log.e(TAG, "getParams: params: " + params);
+//
+//                            return params;
+//                        }
+//
+//                    };
+//
+//                    // Adding request to request queue
+//                    AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+//                }
+//            }
+//        });
     }
 }
